085fbc4b4304df93ffb32143f7d88cf1d29ff14d
Ilya Smelkov <ismelkov@restream.rt.ru>
2018-04-24 15:40:49 +0300

    Init 😘

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">{{ msg }}</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+import map from './grid'
+
+export default Vue.extend({
+  data() {
+    return {
+      msg: map
+    }
+  }
+})
+</script>
