2402fcb27fb02f176e701e757fa549271bfdfb84
Ilya Smelkov <ismelkov@restream.rt.ru>
2018-04-24 17:22:39 +0300

    Block struct 🔔

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,31 @@
 <template>
-  <div id="app">{{ msg }}</div>
+  <div id="app">
+    <div v-for="row in msg">
+      {{ buildRow(row) }}
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 
-import map from './grid'
+import { map, Tile } from './grid'
 
 export default Vue.extend({
   data() {
     return {
       msg: map
+    }
+  },
+  methods: {
+    buildRow(row: Array<Tile>) {
+      return row.map(tile => this.symbol(tile)).join('');
+    },
+    symbol(tile: Tile) {
+      if (tile == ' ') {
+        return '.'
+      }
+      return tile
     }
   }
 })
--- src/grid.ts
+++ src/grid.ts
@@ -1,7 +1,94 @@
-const map: Array<Array<number>> = [
-  [1, 2, 3],
-  [4, 5, 6],
-  [7, 8, 9],
+export type Tile = string;
+type Block = Array<Array<Tile>>
+
+const blocks: Array<Block> = [
+  [
+    ["#", "#"],
+    ["#", " "],
+  ],
+  [
+    ["#", "#"],
+    [" ", "#"],
+  ],
+  [
+    [" ", "#"],
+    ["#", "#"],
+  ],
+  [
+    ["#", " "],
+    ["#", "#"],
+  ],
 ];
 
-export default map;
+const completeBlock: Block = [
+  ["#", "#"],
+  ["#", "#"],
+];
+
+enum MapBlockState {
+  EMPTY,
+  CONFIRMED,
+};
+
+class MapBlock {
+  private state: MapBlockState;
+  public block?: Block;
+
+  constructor() {
+    this.state = MapBlockState.EMPTY;
+  }
+
+  public confirmBlock() {
+    this.state = MapBlockState.CONFIRMED;
+  }
+}
+
+let blockMap: Array<Array<MapBlock>> = [];
+
+const BlockMapWidht = 4;
+const BlockMapHeight = 4;
+const BlockDimension = 2;
+
+for (let i = 0; i < BlockMapHeight; i++) {
+  let row = [];
+
+  for (let j = 0; j < BlockMapWidht; j++) {
+    let block = new MapBlock();
+
+    if (i === 0 || j === 0 || i === BlockMapHeight - 1 || j === BlockMapWidht - 1) {
+      block.confirmBlock();
+      block.block = completeBlock;
+    }
+
+    row.push(block);
+  }
+
+  blockMap.push(row);
+}
+
+function blockMapToMap(blockMap: Array<Array<MapBlock>>) {
+  let map = [];
+
+  for (let i = 0; i < BlockMapHeight; i++) {
+    for (let k = 0; k < BlockDimension; k++) {
+      let row: Tile[] = [];
+
+      for (let j = 0; j < BlockMapWidht; j++) {
+        const block: Block | undefined = blockMap[i][j].block;
+
+        if (block) {
+          row = row.concat(block[k]);
+        } else {
+          const blockRow: string[] = new Array(BlockDimension).fill(" ");
+          row = row.concat(blockRow);
+        }
+      }
+
+      map.push(row);
+    }
+  }
+
+  return map;
+}
+
+export let map: Array<Array<Tile>> = blockMapToMap(blockMap);
