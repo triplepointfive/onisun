a89692cf66cab431cba0e48173b68432ee8177bf
Ilya Smelkov <ismelkov@restream.rt.ru>
2018-04-28 13:49:47 +0300

    Linter ♦

--- src/fov.ts
+++ src/fov.ts
@@ -1,11 +1,11 @@
 export type Visibility = {
-  visible: boolean;
-  degree: number;
-};
+  visible: boolean
+  degree: number
+}
 
 export class Fov {
-  private lightMap: Visibility[][] = [];
-  private doubleRadius: number;
+  private lightMap: Visibility[][] = []
+  private doubleRadius: number
 
   constructor(
     private startx: number,
@@ -15,51 +15,51 @@ export class Fov {
     private height: number,
     private checkSolid: (x: number, y: number) => boolean
   ) {
-    this.doubleRadius = this.radius * this.radius;
+    this.doubleRadius = this.radius * this.radius
   }
 
   public build(): Visibility[][] {
-    this.lightMap = new Array(this.height);
+    this.lightMap = new Array(this.height)
 
     for (let i = 0; i < this.height; i++) {
-      this.lightMap[i] = new Array(this.width);
+      this.lightMap[i] = new Array(this.width)
 
       for (let j = 0; j < this.width; j++) {
-        this.lightMap[i][j] = { visible: true, degree: 0.1 };
+        this.lightMap[i][j] = { visible: true, degree: 0.1 }
       }
     }
 
-    this.lightMap[this.starty][this.startx] = { visible: true, degree: 1 };
+    this.lightMap[this.starty][this.startx] = { visible: true, degree: 1 }
 
     if (!this.checkSolid(this.startx, this.starty)) {
       [[1, 1], [1, -1], [-1, 1], [-1, -1]].forEach(([dx, dy]) => {
-        this.castLight(1, 1.0, 0.0, 0, dx, dy, 0);
-        this.castLight(1, 1.0, 0.0, dx, 0, 0, dy);
-      });
+        this.castLight(1, 1.0, 0.0, 0, dx, dy, 0)
+        this.castLight(1, 1.0, 0.0, dx, 0, 0, dy)
+      })
     }
 
-    return this.lightMap;
+    return this.lightMap
   }
 
   private castLight(row: number, start: number, end: number, xx: number, xy: number, yx: number, yy: number) {
-    if (start < end) { return; }
+    if (start < end) { return }
 
-    let newStart = 0.0;
-    let blocked = false;
+    let newStart = 0.0
+    let blocked = false
 
     for (let distance = row; distance <= this.radius && !blocked; distance++) {
-      let deltaY = -distance;
+      let deltaY = -distance
 
       for (let deltaX: number = -distance; deltaX <= 0; deltaX++) {
-        let currentX = Math.round(this.startx + deltaX * xx + deltaY * xy);
-        let currentY = Math.round(this.starty + deltaX * yx + deltaY * yy);
-        let leftSlope = (deltaX - 0.5) / (deltaY + 0.5);
-        let rightSlope = (deltaX + 0.5) / (deltaY - 0.5);
+        let currentX = Math.round(this.startx + deltaX * xx + deltaY * xy)
+        let currentY = Math.round(this.starty + deltaX * yx + deltaY * yy)
+        let leftSlope = (deltaX - 0.5) / (deltaY + 0.5)
+        let rightSlope = (deltaX + 0.5) / (deltaY - 0.5)
 
         if (!(currentX >= 0 && currentY >= 0 && currentX < this.width && currentY < this.height) || start < rightSlope) {
-          continue;
+          continue
         } else if (end > leftSlope) {
-          break;
+          break
         }
 
         // check if it's within the lightable area and light if needed
@@ -67,21 +67,21 @@ export class Fov {
           this.lightMap[currentY][currentX] = {
             visible: true,
             degree: 1 - this.doubleDistance(deltaX, deltaY) / this.doubleRadius
-          };
+          }
         }
 
         if (blocked) { // previous cell was a blocking one
           if (this.checkSolid(currentX, currentY)) { // hit a wall
-            newStart = rightSlope;
+            newStart = rightSlope
           } else {
-            blocked = false;
-            start = newStart;
+            blocked = false
+            start = newStart
           }
         } else {
           if (this.checkSolid(currentX, currentY) && distance < this.radius) { // hit a wall within sight line
-              blocked = true;
-              this.castLight(distance + 1, start, leftSlope, xx, xy, yx, yy);
-              newStart = rightSlope;
+              blocked = true
+              this.castLight(distance + 1, start, leftSlope, xx, xy, yx, yy)
+              newStart = rightSlope
           }
         }
       }
@@ -89,6 +89,6 @@ export class Fov {
   }
 
   private doubleDistance(x: number, y: number): number {
-    return x * x + y * y;
+    return x * x + y * y
   }
 }
--- src/grid.ts
+++ src/grid.ts
@@ -1,6 +1,6 @@
-import * as _ from "lodash";
+import * as _ from 'lodash'
 
-type BlockId = string;
+type BlockId = string
 
 export enum TileTypes {
   Wall,
@@ -9,109 +9,109 @@ export enum TileTypes {
 }
 
 export class Tile {
-  private static repository: { [key: string]: Tile } = {};
+  private static repository: { [key: string]: Tile } = {}
 
   private static register(key: string, tile: Tile) {
     if (this.repository[key]) {
-      throw `Tile '${key}' is already registered!`;
+      throw `Tile '${key}' is already registered!`
     }
 
-    this.repository[key] = tile;
+    this.repository[key] = tile
   }
 
   public static retrive(key: string): Tile {
-    let tile = this.repository[key];
+    let tile = this.repository[key]
 
     if (!tile) {
-      throw `Tile '${key}' is not registered!`;
+      throw `Tile '${key}' is not registered!`
     }
 
-    return tile;
+    return tile
   }
 
   // TODO: rename type to kind
   constructor(public key: string, public display: string, public type: TileTypes) {
-    Tile.register(key, this);
+    Tile.register(key, this)
   }
 
   public visibleThrough(): boolean {
-    return this.type === TileTypes.Floor;
+    return this.type === TileTypes.Floor
   }
-};
+}
 
-new Tile("R", " ", TileTypes.Floor);
-new Tile("C", " ", TileTypes.Floor);
-new Tile("W", "#", TileTypes.Wall);
-new Tile("D", "+", TileTypes.Door);
+new Tile('R', ' ', TileTypes.Floor)
+new Tile('C', ' ', TileTypes.Floor)
+new Tile('W', '#', TileTypes.Wall)
+new Tile('D', '+', TileTypes.Door)
 
 export class Block {
-  public static readonly Dimensions = 3;
+  public static readonly Dimensions = 3
 
-  public id: BlockId;
+  public id: BlockId
 
   constructor(public content: Tile[][], public weight: number) {
     if (content.length !== Block.Dimensions || content.some(row => row.length !== Block.Dimensions)) {
-      throw `Invalid block '${content}'`;
+      throw `Invalid block '${content}'`
     }
 
-    this.id = content.map(row => row.map(tile => tile.key).join("")).join("");
+    this.id = content.map(row => row.map(tile => tile.key).join('')).join('')
   }
 
   public matchRight(block: Block): boolean {
-    return this.matchHorizontal(this, block);
+    return this.matchHorizontal(this, block)
   }
 
   public matchLeft(block: Block): boolean {
-    return this.matchHorizontal(block, this);
+    return this.matchHorizontal(block, this)
   }
 
   public matchTop(block: Block): boolean {
-    return this.matchVertical(block, this);
+    return this.matchVertical(block, this)
   }
 
   public matchBootom(block: Block): boolean {
-    return this.matchVertical(this, block);
+    return this.matchVertical(this, block)
   }
 
   public mirrorVertically(): Block {
-    return new Block(_.cloneDeep(this.content).reverse(), this.weight);
+    return new Block(_.cloneDeep(this.content).reverse(), this.weight)
   }
 
   public mirrorHorizontally(): Block {
-    return new Block(_.cloneDeep(this.content).map(row => row.reverse()), this.weight);
+    return new Block(_.cloneDeep(this.content).map(row => row.reverse()), this.weight)
   }
 
   public rotateRight(): Block {
-    return new Block(this.rotate(_.cloneDeep(this.content)), this.weight);
+    return new Block(this.rotate(_.cloneDeep(this.content)), this.weight)
   }
 
   private rotate(matrix: any[][]): any[][] {
-    matrix = matrix.reverse();
+    matrix = matrix.reverse()
 
-    let temp: number;
+    let temp: number
     for (let i = 0; i < matrix.length; i++) {
       for (let j = 0; j < i; j++) {
-        temp = matrix[i][j];
-        matrix[i][j] = matrix[j][i];
-        matrix[j][i] = temp;
+        temp = matrix[i][j]
+        matrix[i][j] = matrix[j][i]
+        matrix[j][i] = temp
       }
     }
 
-    return matrix;
+    return matrix
   }
 
   private matchHorizontal(left: Block, right: Block): boolean {
     return _.isEqual(
       left.content.map(row => row[Block.Dimensions - 1]),
       right.content.map(row => row[0]),
-    );
+    )
   }
 
   private matchVertical(top: Block, bottom: Block): boolean {
     return _.isEqual(
       top.content[Block.Dimensions - 1],
       bottom.content[0],
-    );
+    )
   }
 }
 
@@ -120,93 +120,93 @@ enum Direction {
   Bottom,
   Left,
   Right,
-};
+}
 
 interface Neighbors {
-  [key: string]: Block[];
+  [key: string]: Block[]
 }
 
 export class BlockRepository {
-  public blocks: Block[] = [];
-  public uniqBlockIds: Set<string> = new Set();
+  public blocks: Block[] = []
+  public uniqBlockIds: Set<string> = new Set()
 
-  public neighbors: { [key: string]: Neighbors } = {};
+  public neighbors: { [key: string]: Neighbors } = {}
 
   constructor(public borderBlock: Block) {
-    this.addBlock(borderBlock);
+    this.addBlock(borderBlock)
   }
 
   public addBlock(block: Block, mirrored: boolean = false): void {
     if (this.uniqBlockIds.has(block.id)) {
-      return;
+      return
     }
 
-    this.blocks.push(block);
-    this.uniqBlockIds.add(block.id);
+    this.blocks.push(block)
+    this.uniqBlockIds.add(block.id)
 
-    this.findNeighbors(block);
+    this.findNeighbors(block)
 
     if (!mirrored) {
-      const horizontalBlock = block.mirrorHorizontally();
+      const horizontalBlock = block.mirrorHorizontally()
 
-      this.addBlock(horizontalBlock, true);
-      this.addBlock(block.mirrorVertically(), true);
-      this.addBlock(horizontalBlock.mirrorVertically(), true);
+      this.addBlock(horizontalBlock, true)
+      this.addBlock(block.mirrorVertically(), true)
+      this.addBlock(horizontalBlock.mirrorVertically(), true)
     }
 
-    let rotationBlock = block.rotateRight();
+    let rotationBlock = block.rotateRight()
     for (let i = 0; i < 3; i++) {
-      this.addBlock(rotationBlock, true);
-      rotationBlock = rotationBlock.rotateRight();
+      this.addBlock(rotationBlock, true)
+      rotationBlock = rotationBlock.rotateRight()
     }
   }
 
   public available(top: Block | undefined, bottom: Block | undefined,
                    left: Block | undefined, right: Block | undefined): Block[] {
-    let availableBlocks = this.blocks;
+    let availableBlocks = this.blocks
 
     if (top !== undefined) {
-      availableBlocks = _.intersection(availableBlocks, this.neighbors[top.id][Direction.Bottom]);
+      availableBlocks = _.intersection(availableBlocks, this.neighbors[top.id][Direction.Bottom])
     }
 
     if (bottom !== undefined) {
-      availableBlocks = _.intersection(availableBlocks, this.neighbors[bottom.id][Direction.Top]);
+      availableBlocks = _.intersection(availableBlocks, this.neighbors[bottom.id][Direction.Top])
     }
 
     if (left !== undefined) {
-      availableBlocks = _.intersection(availableBlocks, this.neighbors[left.id][Direction.Right]);
+      availableBlocks = _.intersection(availableBlocks, this.neighbors[left.id][Direction.Right])
     }
 
     if (right !== undefined) {
-      availableBlocks = _.intersection(availableBlocks, this.neighbors[right.id][Direction.Left]);
+      availableBlocks = _.intersection(availableBlocks, this.neighbors[right.id][Direction.Left])
     }
 
-    return _.sortBy(availableBlocks, block => Math.random() * block.weight);
-    // return _.sortBy(availableBlocks, block => 1 * block.weight).reverse();
+    // return _.sortBy(availableBlocks, block => Math.random() * block.weight)
+    return _.sortBy(availableBlocks, block => 1 * block.weight).reverse()
   }
 
   private findNeighbors(centralBlock: Block) {
     this.blocks.forEach((block) => {
       if (centralBlock.matchTop(block)) {
-        this.addNeighbor(centralBlock, block, Direction.Top);
-        this.addNeighbor(block, centralBlock, Direction.Bottom);
+        this.addNeighbor(centralBlock, block, Direction.Top)
+        this.addNeighbor(block, centralBlock, Direction.Bottom)
       }
 
       if (centralBlock.matchBootom(block)) {
-        this.addNeighbor(centralBlock, block, Direction.Bottom);
-        this.addNeighbor(block, centralBlock, Direction.Top);
+        this.addNeighbor(centralBlock, block, Direction.Bottom)
+        this.addNeighbor(block, centralBlock, Direction.Top)
       }
 
       if (centralBlock.matchLeft(block)) {
-        this.addNeighbor(centralBlock, block, Direction.Left);
-        this.addNeighbor(block, centralBlock, Direction.Right);
+        this.addNeighbor(centralBlock, block, Direction.Left)
+        this.addNeighbor(block, centralBlock, Direction.Right)
       }
 
       if (centralBlock.matchRight(block)) {
-        this.addNeighbor(centralBlock, block, Direction.Right);
-        this.addNeighbor(block, centralBlock, Direction.Left);
+        this.addNeighbor(centralBlock, block, Direction.Right)
+        this.addNeighbor(block, centralBlock, Direction.Left)
       }
-    });
+    })
   }
 
   private addNeighbor(central: Block, neighbor: Block, direction: Direction) {
@@ -216,148 +216,148 @@ export class BlockRepository {
         [Direction.Bottom]: [],
         [Direction.Left]: [],
         [Direction.Right]: [],
-      };
+      }
     }
 
-    this.neighbors[central.id][direction].push(neighbor);
+    this.neighbors[central.id][direction].push(neighbor)
   }
 }
 
 enum MapBlockState {
   EMPTY,
   CONFIRMED,
-};
+}
 
 class MapBlock {
-  private state: MapBlockState;
-  public block?: Block;
+  private state: MapBlockState
+  public block?: Block
 
   constructor() {
-    this.state = MapBlockState.EMPTY;
+    this.state = MapBlockState.EMPTY
   }
 
   public isFinished(): boolean {
-    return this.block !== undefined && this.state === MapBlockState.CONFIRMED;
+    return this.block !== undefined && this.state === MapBlockState.CONFIRMED
   }
 
   public confirmBlock() {
-    this.state = MapBlockState.CONFIRMED;
+    this.state = MapBlockState.CONFIRMED
   }
 }
 
 class Random {
   public static addDoor(): boolean {
-    return Math.random() > 0.5;
+    return Math.random() > 0.5
   }
 }
 
 export class Map {
-  private blockMap: MapBlock[][] = [];
-  private map: Tile[][] = [];
+  private blockMap: MapBlock[][] = []
+  private map: Tile[][] = []
 
-  private steps = 0;
+  private steps = 0
 
   constructor(public width: number, public height: number, private blockRepository: BlockRepository) {
-    this.initializeBlockMap();
+    this.initializeBlockMap()
   }
 
   public initializeBlockMap() {
     for (let i = 0; i < this.height; i++) {
-      let row = [];
+      let row = []
 
       for (let j = 0; j < this.width; j++) {
-        let block = new MapBlock();
+        let block = new MapBlock()
 
         if (i === 0 || j === 0 || i === this.height - 1 || j === this.width - 1) {
-          block.confirmBlock();
-          block.block = this.blockRepository.borderBlock;
+          block.confirmBlock()
+          block.block = this.blockRepository.borderBlock
         }
 
-        row.push(block);
+        row.push(block)
       }
 
-      this.blockMap.push(row);
+      this.blockMap.push(row)
     }
   }
 
   public toMap(): Tile[][] {
-    return this.map;
+    return this.map
   }
 
   public fillMap(i: number, j: number): boolean {
     if (this.blockMap[i][j].block !== undefined) {
-      return true;
+      return true
     }
-    this.steps += 1;
+    this.steps += 1
 
     if (this.steps > 100000) {
-      throw "failed to generate";
+      throw 'failed to generate'
     }
 
-    const top = this.blockMap[i - 1][j].block;
-    const bottom = this.blockMap[i + 1][j].block;
-    const left = this.blockMap[i][j - 1].block;
-    const right = this.blockMap[i][j + 1].block;
+    const top = this.blockMap[i - 1][j].block
+    const bottom = this.blockMap[i + 1][j].block
+    const left = this.blockMap[i][j - 1].block
+    const right = this.blockMap[i][j + 1].block
 
-    const shuffledBlocks = this.blockRepository.available(top, bottom, left, right);
+    const shuffledBlocks = this.blockRepository.available(top, bottom, left, right)
     for (let k = 0; k < shuffledBlocks.length; k++) {
-      const block: Block = shuffledBlocks[k];
+      const block: Block = shuffledBlocks[k]
 
-      this.blockMap[i][j].block = block;
+      this.blockMap[i][j].block = block
 
       const state = this.fillMap(i - 1, j)
                  && this.fillMap(i, j - 1)
                  && this.fillMap(i + 1, j)
-                 && this.fillMap(i, j + 1);
+                 && this.fillMap(i, j + 1)
 
       if (state) {
-        break;
+        break
       }
       else {
-        this.blockMap[i][j].block = undefined;
+        this.blockMap[i][j].block = undefined
       }
     }
 
-    return this.blockMap[i][j].block !== undefined;
+    return this.blockMap[i][j].block !== undefined
   }
 
   public postProcess(): void {
-    this.buildMap();
-    this.addDoors();
+    this.buildMap()
+    this.addDoors()
   }
 
   private buildMap(): void {
-    const iterateHeight = this.height - 1;
-    const iterateWidth = this.width - 1;
+    const iterateHeight = this.height - 1
+    const iterateWidth = this.width - 1
 
     for (let i = 1; i < iterateHeight; i++) {
       for (let k = 0; k < Block.Dimensions - (i === iterateHeight - 1 ? 0 : 1); k++) {
-        let row: Tile[] = [];
+        let row: Tile[] = []
 
         for (let j = 1; j < iterateWidth; j++) {
-          const block: Block | undefined = this.blockMap[i][j].block;
+          const block: Block | undefined = this.blockMap[i][j].block
 
           if (block) {
-            const blockRow = block.content[k];
-            row = row.concat(j === iterateWidth - 1 ? blockRow : _.initial(blockRow));
+            const blockRow = block.content[k]
+            row = row.concat(j === iterateWidth - 1 ? blockRow : _.initial(blockRow))
           } else {
-            const blockRow: Tile[] = new Array(Block.Dimensions).fill(" ");
-            row = row.concat(blockRow);
+            const blockRow: Tile[] = new Array(Block.Dimensions).fill(' ')
+            row = row.concat(blockRow)
           }
         }
 
-        this.map.push(row);
+        this.map.push(row)
       }
     }
   }
 
   private addDoors(): void {
     for (let i = 1; i < this.map.length; i++) {
-      const row: Tile[] = this.map[i];
+      const row: Tile[] = this.map[i]
 
       for (let j = 1; j < row.length; j++) {
-        if (row[j].key === "C" && (this.map[i][j - 1].key === "R" || this.map[i - 1][j].key === "R") && Random.addDoor()) {
-          row[j] = Tile.retrive("D");
+        if (row[j].key === 'C' && (this.map[i][j - 1].key === 'R' || this.map[i - 1][j].key === 'R') && Random.addDoor()) {
+          row[j] = Tile.retrive('D')
         }
       }
     }
