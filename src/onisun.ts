03c873f427445c71616a9691ee3fdd30a108a971
Ilya Smelkov <ismelkov@restream.rt.ru>
2018-06-14 13:47:45 +0300

    Added pool 🍺

--- src/onisun.ts
+++ src/onisun.ts
@@ -14,7 +14,7 @@ import { Dispatcher } from './ai'
 import { Weapon, Item } from './items'
 import { LevelMap } from './map'
 
-import { sample } from 'lodash'
+import { random } from 'lodash'
 
 export type GeneratorOptions = {
   minSize: number
@@ -24,21 +24,52 @@ export type GeneratorOptions = {
   addDoors: boolean
 }
 
+class Pool<T> {
+  private totalWeight: number = 0
+  private items: [number, T][] = []
+
+  constructor(items: [number, T][]) {
+    items.forEach(([weight, item]) => this.add(weight, item))
+  }
+
+  public add(weight: number, item: T): void {
+    if (weight < 1) {
+      throw `Item '${item}' weight is lower than 1`
+    }
+
+    this.items.push([weight, item])
+    this.totalWeight += Math.ceil(weight)
+  }
+
+  public pick(): T {
+    if (this.items.length === 0) {
+      throw 'Tried to use empty pool'
+    }
+
+    let pick = random(this.totalWeight - 1)
+
+    return this.items.find(([weight, item]) => {
+      pick -= weight
+
+      return pick <= 0
+    })[1]
+  }
+}
+
 export class Game {
   public player: Creature
   public map: LevelMap
   public itemsPool: Item[]
-  public weapons: Weapon[]
 
   constructor(generatorOptions: GeneratorOptions) {
     const radius = 5
 
-    this.weapons = [
-      new Weapon('Katana', 10),
-      new Weapon('Axe', 7),
-      new Weapon('Dagger', 3),
-      new Weapon('Hammer', 5),
-    ]
+    const weapons = new Pool([
+      [1, new Weapon('Katana', 10)],
+      [1, new Weapon('Axe', 7)],
+      [1, new Weapon('Dagger', 3)],
+      [3, new Weapon('Hammer', 5)],
+    ])
 
     this.map = generate(
       50,
@@ -66,7 +97,7 @@ export class Game {
 
     this.player = new Creature(x, y, 1, 4, 20, radius, 101, Clan.PlayerOnlyEnemy, new Dispatcher())
     this.player.addToMap(this.map)
-    this.player.putOn(sample(this.weapons))
+    this.player.putOn(weapons.pick())
 
     x = this.map.width - 1
     y = this.map.height - 1
@@ -82,14 +113,14 @@ export class Game {
 
     const creature2 = new Creature(x, y, 1, 4, 5, radius, 100, Clan.PlayerOnlyEnemy, new Dispatcher())
     creature2.addToMap(this.map)
-    creature2.putOn(sample(this.weapons))
+    creature2.putOn(weapons.pick())
 
     const creature3 = new Creature(x - 1, y, 1, 4, 5, radius, 100, Clan.PlayerOnlyEnemy, new Dispatcher())
     creature3.addToMap(this.map)
-    creature3.putOn(sample(this.weapons))
+    creature3.putOn(weapons.pick())
 
     const creature4 = new Creature(x, y - 1, 1, 4, 5, radius, 100, Clan.PlayerOnlyEnemy, new Dispatcher())
     creature4.addToMap(this.map)
-    creature4.putOn(sample(this.weapons))
+    creature4.putOn(weapons.pick())
   }
 }
