1d6fe633c0823a1363deb04ddc7a5906ab5ca925
Ilya Smelkov <ismelkov@restream.rt.ru>
2018-06-25 18:39:48 +0300

    Better dungeon 🎃

--- src/onisun.ts
+++ src/onisun.ts
@@ -64,27 +64,90 @@ const itemsPool = new Pool<null, Item>([
   [10, () => new BodyArmor('Роба', new Modifier({ defense: 1 }))],
 ])
 
-const creaturesPool = new Pool<null, Creature>([
-  [
-    1,
-    () => {
+const newCreature = (characteristics: Characteristics, name: string) => {
       return new Creature(
+        characteristics,
+        Clan.PlayerOnlyEnemy,
+        new Dispatcher(),
+        name
+      )
+}
+
+const rat = () => {
+  return newCreature(
         new Characteristics({
           attack: 1,
           defense: 1,
           dexterity: 1,
           health: 1,
           radius: 5,
+          speed: 110,
+        }),
+        'Rat',
+   )
+}
+
+const orc = () => {
+  return newCreature(
+        new Characteristics({
+          attack: 3,
+          defense: 3,
+          dexterity: 2,
+          health: 5,
+          radius: 5,
           speed: 100,
         }),
+        'Orc',
+   )
+}
 
-        Clan.PlayerOnlyEnemy,
-        new Dispatcher(),
-        'Rat',
-      )
-    },
-  ],
-])
+const undead = () => {
+  return newCreature(
+        new Characteristics({
+          attack: 2,
+          defense: 7,
+          dexterity: 1,
+          health: 10,
+          radius: 5,
+          speed: 80,
+        }),
+        'Undead',
+   )
+}
+
+const robot = () => {
+  return newCreature(
+        new Characteristics({
+          attack: 5,
+          defense: 5,
+          dexterity: 3,
+          health: 7,
+          radius: 7,
+          speed: 110,
+        }),
+        'Robot',
+   )
+}
+
+const dragon = () => {
+  return newCreature(
+        new Characteristics({
+          attack: 7,
+          defense: 7,
+          dexterity: 7,
+          health: 30,
+          radius: 5,
+          speed: 100,
+        }),
+        'Dragon',
+   )
+}
+
+const creaturesPool1 = new Pool<null, Creature>([ [ 1, rat, ], ])
+const creaturesPool2 = new Pool<null, Creature>([ [ 1, rat, ], [2, orc ]])
+const creaturesPool3 = new Pool<null, Creature>([ [ 1, rat, ], [ 1, orc], [2, undead] ])
+const creaturesPool4 = new Pool<null, Creature>([ [ 2, undead ], [ 2, robot ] ])
+const creaturesPool5 = new Pool<null, Creature>([ [ 2, robot, ], [1, dragon ] ])
 
 export class Onisun extends Game {
   public player: Creature
@@ -100,11 +163,16 @@ export class Onisun extends Game {
     let map4 = this.generateMap(generatorOptions)
     let map5 = this.generateMap(generatorOptions)
 
+    addCreatures(0.05, map1, creaturesPool1)
+    addCreatures(0.06, map2, creaturesPool2)
+    addCreatures(0.07, map3, creaturesPool3)
+    addCreatures(0.08, map4, creaturesPool4)
+    addCreatures(0.09, map5, creaturesPool5)
+
     connectMaps(map1, map2)
     connectMaps(map2, map3)
     connectMaps(map3, map4)
     connectMaps(map4, map5)
-    connectMaps(map5, map1)
 
     this.currentMap = map1
 
@@ -161,7 +229,6 @@ export class Onisun extends Game {
     centralize(map)
     map.game = this
 
-    addCreatures(0.05, map, creaturesPool)
     addItems(0.01, map, weapons.merge(itemsPool))
 
     return map
