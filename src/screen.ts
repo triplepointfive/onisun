e15461bc328d4d5b019ab5c2a54d7e23550a2d53
Ilya Smelkov <triplepointfive@gmail.com>
2018-07-30 20:29:36 +0300

    Pickup screen ✴

--- src/screen.ts
+++ src/screen.ts
@@ -41,6 +41,7 @@ export enum IdleInputKey {
   Handle,
 
   Inventory,
+  PickUp,
 }
 
 export class IdleScreen extends Screen {
@@ -74,6 +75,10 @@ export class IdleScreen extends Screen {
     case IdleInputKey.Handle:
       return new AIHandleEnvEvent().act(this.game.player)
 
+    case IdleInputKey.PickUp:
+      this.game.screen = new PickUpScreen(this.game)
+      return
+
     case IdleInputKey.Inventory:
       this.game.screen = new InventoryScreen(this.game)
       return
@@ -86,8 +91,11 @@ export enum InventoryInputKey {
 }
 
 export class InventoryScreen extends Screen {
+  public positions: string[]
+
   constructor(game: Game) {
     super(ScreenType.Inventory, game)
+    this.initPositions()
   }
 
   public onInput(key: InventoryInputKey) {
@@ -95,7 +103,22 @@ export class InventoryScreen extends Screen {
       case InventoryInputKey.Close:
         return this.game.screen = new IdleScreen(this.game)
     }
+  }
 
+  protected initPositions(): void {
+
+  }
+}
+
+export class PickUpScreen extends InventoryScreen {
+  protected initPositions(): void {
+    const items = this.player.currentLevel.at(this.player.pos.x, this.player.pos.y).items
+
+    if (items) {
+      this.positions = items.bunch.map(itemGroup => itemGroup.item.name)
+    } else {
+      this.positions = ['No items']
+    }
   }
 }
 
