287347b51f4a56f1460018d2bea90586cb0d45ac
Ilya Smelkov <ismelkov@restream.rt.ru>
2018-10-25 17:19:43 +0300

    Events listening 🚥

--- e-OniSun/src/Main.elm
+++ e-OniSun/src/Main.elm
@@ -1,9 +1,10 @@
+import Array
 import Browser
+import Browser.Events exposing (onKeyPress)
 import Html exposing (..)
+import Json.Decode as Decode
 
 import Model.Talent exposing (..)
-
-import Array
 
 main =
   Browser.element
@@ -13,22 +14,33 @@ main =
     , view = view
     }
 
-type alias Model = Int
+type alias Model = { levelMap : LevelMap }
 
-type alias Msg = Int
+type alias Msg = String
 
-init () = (0, Cmd.none)
+init () = ({ levelMap = levelMap }, Cmd.none)
 
-update msg _ = (0, Cmd.none)
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+  case msg of
+    "k" ->
+      ({ model | levelMap = withTile ({ x = 0, y = 0 }) (\tile -> { tile | creature = Just Creature }) model.levelMap }
+      , Cmd.none
+      )
+    _ -> (model, Cmd.none)
+
+keyDecoder : Decode.Decoder String
+keyDecoder =
+  Decode.field "key" Decode.string
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-  Sub.none
+  onKeyPress keyDecoder
+
 
 levelMap : LevelMap
 levelMap =
-  withTile ({ x = 0, y = 0 }) (\tile -> { tile | creature = Just Creature })
-  <| fromString """
+  fromString """
 #####
 #   #
 #   #
@@ -40,7 +52,7 @@ levelMap =
 
 view : Model -> Html Msg
 view model =
-  pre [] (List.map (\row -> div [] <| List.map (tileToElem) row) (raw levelMap))
+  pre [] (List.map (\row -> div [] <| List.map (tileToElem) row) (raw model.levelMap))
 
 raw : LevelMap -> List (List Tile)
 raw rec = Array.toList <| Array.map (Array.toList) rec.map
